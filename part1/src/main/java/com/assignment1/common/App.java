b260ccb8a785f57dad51e13f2c6ed1a9be177c29
Robert Adler <robert.adler.98@gmail.com>
2018-10-04 18:56:52 -0400

    Updated Part 1 to use Category as its Collector

--- part1/src/main/java/com/assignment1/common/App.java
+++ part1/src/main/java/com/assignment1/common/App.java
@@ -7,14 +7,14 @@ public class App {
 
 	public static void main( String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
-		Collection collection = (Collection)context.getBean("collection");	
-		System.out.print("----Printing Values From List-----");
-		collection.printBookList();
-		System.out.print("----Printing Values From Map-----");
-		collection.printBookMap();
-		System.out.print("----Printing Values From Set-----");
-		collection.printBookSet();
-		
+		//Collection collection = (Collection)context.getBean("collection");	
+		Category category = (Category) context.getBean("Category");
+		category.printCategory();
+		category.printBookMap();
+		category.printCategory();
+		category.printBookList();
+		category.printCategory();
+		category.printBookSet();
 		
 	}
 }
--- part1/src/main/java/com/assignment1/common/Category.java
+++ part1/src/main/java/com/assignment1/common/Category.java
@@ -3,6 +3,7 @@ package com.assignment1.common;
 import java.util.Set;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Category {
 	
@@ -10,6 +11,82 @@ public class Category {
 	public String name;
 	public Set<Book>booksSet;
 	public List<Book>booksList;
-	public Map<String, Book>booksMap;
+	public Map<String, Book> booksMap;
+	
+	public Category() {};
+	public Category(List<Book> bookList, Set<Book> bookSet, Map<String, Book> bookMap, Long id, String name) {
+		this.booksList = bookList;
+		this.booksSet = bookSet;
+		this.booksMap = bookMap;
+		this.id = id;
+		this.name = name; 
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void printCategory() {
+		System.out.println("Id: " + id + " Category Name " + name);
+	}
+ 	
+	public Map<String, Book> getBookMap() {
+		return booksMap;
+	}
+	
+	public void  setBookMap(Map<String, Book> bookMap) {
+		this.booksMap = bookMap; 
+	}
+	
+	public void printBookMap() {
+		for(Entry<String, Book> entry : booksMap.entrySet()) {
+			Book book = entry.getValue();
+			book.printBook();
+		}
+	}
+	
+	public Set<Book> getBookSet() {
+		return booksSet;
+	}
+	
+	public void setBookSet(Set<Book> bookSet) {
+		this.booksSet = bookSet;
+	}
+	
+	public void printBookSet() {
+		for (Book book : booksSet) {
+			book.printBook();			
+		}
+	}
+	
+	
+	public List<Book> getBookList() {
+		return booksList;
+	}
+	
+	public void setBookList(List<Book> bookList) {
+		this.booksList = bookList;
+	}
+	
+	public void printBookList() {
+		for (Book book : booksList ) {
+			book.printBook(); 
+		}
+	}
+
+	
+	
 
 }
